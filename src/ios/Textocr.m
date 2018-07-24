4f36131192e41a9339dfa368f32af21c1c243d7a
Christy Thomas Alex <34361280+ChrisTomAlx@users.noreply.github.com>
2018-07-24 13:13:50 +0530

    Removed NSLogs

--- src/ios/Textocr.m
+++ src/ios/Textocr.m
@@ -24,33 +24,23 @@
         NSString* name;
         self.image = NULL;
         @try {
-            NSLog(@"%s","Iniiiiiiii!!!");
-            
-            //stype = (int)[[_commandglo arguments] objectAtIndex:0];
             NSString *st = [[_commandglo arguments] objectAtIndex:0];
-            NSLog(@"st : %@",st);
             stype = [st intValue];
-            //stype = 2;
-            NSLog(@"stype : %d",stype);
             // 0 NORMFILEURI
             // 1 NORMNATIVEURI
             // 2 FASTFILEURI
             // 3 FASTNATIVEURI
             // 4 BASE64
             
-            //rtype = (int)[[_commandglo arguments] objectAtIndex:1];
             NSString *rt = [[_commandglo arguments] objectAtIndex:1];
-            NSLog(@"rt : %@",rt);
             rtype = [rt intValue];
-            NSLog(@"rtype : %d",rtype);
             // 0 BLOCKS
             // 1 LINES
             // 2 WORDS
             // 3 ALL
             
             name = [[_commandglo arguments] objectAtIndex:2];
-            //Imagedata that is either a uri or base64
-            NSLog(@"%s","Ini done!!!");
+
         }
         @catch (NSException *exception) {
             CDVPluginResult* result = [CDVPluginResult
@@ -61,39 +51,16 @@
         
         if (stype == NORMFILEURI || stype == NORMNATIVEURI || stype == FASTFILEURI || stype == FASTNATIVEURI)
         {
-            NSLog(@"%s","file mesertyuiosing!!!");
-            //            @try // remove file:// from outputs of camera plugin and scan plugin to do
-            //            {
-            //                NSString *filesubString = [name substringWithRange:NSMakeRange(0,7)];
-            //                NSLog(@"filesubString: %@", filesubString);
-            //                if ([filesubString  isEqual: @("file://")]) {
-            //                    //name = [name stringByReplacingCharactersInRange:NSMakeRange(0,7) withString:@("")];
-            //                }
-            //                NSLog(@"NAMEURL: %@", name);
-            //            }
-            //            @catch (NSException *exception) {
-            //                CDVPluginResult* result = [CDVPluginResult
-            //                                           resultWithStatus:CDVCommandStatus_ERROR
-            //                                           messageAsString:@"uri Or Base string manipulation error"];
-            //                [self.commandDelegate sendPluginResult:result callbackId:_commandglo.callbackId];
-            //            }
-            NSLog(@"NAMEURL: %@", name);
-            NSLog(@"%s","what am i??!!!");
             if (stype==NORMFILEURI)
             {
-                NSLog(@"%s","JUST NORMAL!!!");
                 NSData *imageData = [NSData dataWithContentsOfURL:[NSURL URLWithString:name]];
                 self.image = [UIImage imageWithData:imageData];
-                NSLog(@"%s","OR MAYBE NOTT!!!");
             }
             else if (stype==NORMNATIVEURI)
             {
                 NSString *urlString = [NSString stringWithFormat:@"%@", name];
                 NSURL *url = [NSURL URLWithString:[urlString stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]]];
-                NSLog(@"urlmediaiswahwah :- (%@)", url);
                 NSData *imageData = [self retrieveAssetDataPhotosFramework:url];
-                NSLog(@"urlmediaiswahwah :- (%@)", imageData);
-                NSLog(@"%s","NORMNATIVEURI huhu!!!");
                 self.image = [UIImage imageWithData:imageData];
             }
             else if (stype==FASTFILEURI)
@@ -106,9 +73,7 @@
             {
                 NSString *urlString = [NSString stringWithFormat:@"%@", name];
                 NSURL *url = [NSURL URLWithString:[urlString stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]]];
-                
                 NSData *imageData = [self retrieveAssetDataPhotosFramework:url];
-                NSLog(@"%s","NORMNATIVEURI huhu!!!");
                 self.image = [UIImage imageWithData:imageData];
                 self.image = [self resizeImage:self.image];
             }
@@ -133,7 +98,7 @@
             self.textDetector = [GMVDetector detectorOfType:GMVDetectorTypeText options:nil];
             NSArray<GMVTextBlockFeature *> *features = [self.textDetector featuresInImage:self.image
                                                                                   options:nil];
-            NSLog(@"%s","DETECTORRRRRR!!!");
+            
             int count = 0;
             NSMutableString* blocks = [NSMutableString string];
             NSMutableString* lines = [NSMutableString string];
@@ -142,32 +107,18 @@
             // Iterate over each text block.
             for (GMVTextBlockFeature *textBlock in features) {
                 count++;
-                
-                NSLog(@"Text Block: %@", NSStringFromCGRect(textBlock.bounds));
-                NSLog(@"lang: %@ value: %@", textBlock.language, textBlock.value);
-                
                 [blocks appendString:[textBlock.value mutableCopy]];
                 [blocks appendString:@"\n"];
                 [blocks appendString:@"\n"];
-                NSLog(@"blocks: %@",blocks);
+                
                 // For each text block, iterate over each line.
-                for (GMVTextLineFeature *textLine in textBlock.lines) {
-                    NSLog(@"Text Line: %@", NSStringFromCGRect(textLine.bounds));
-                    NSLog(@"lang: %@ value: %@", textLine.language, textLine.value);
-                    
+                for (GMVTextLineFeature *textLine in textBlock.lines) {               
                     [lines appendString:[textLine.value mutableCopy]];
                     [lines appendString:@"\n"];
-                    NSLog(@"lines: %@",lines);
-                    
                     // For each line, iterate over each word.
                     for (GMVTextElementFeature *textElement in textLine.elements) {
-                        NSLog(@"Text Element: %@", NSStringFromCGRect(textElement.bounds));
-                        NSLog(@"value: %@", textElement.value);
-                        
                         [words appendString:[textElement.value mutableCopy]];
-                        [words appendString:@","];
-                        NSLog(@"words: %@",words);
-                        
+                        [words appendString:@","];  
                     }
                 }
             }
@@ -178,29 +129,20 @@
                 [self.commandDelegate sendPluginResult:result callbackId:_commandglo.callbackId];
             }
             else {
-                NSLog(@"%s","It worked!!!");
                 NSString* message;
                 if (rtype==BLOCKS)
                 {
-                    NSLog(@"%d",rtype);
-                    
                     message = blocks;
-                    NSLog(@"%@",blocks);
-                    NSLog(@"%@",message);
                 }
                 
                 else if (rtype==LINES)
                 {
                     message = lines;
-                    NSLog(@"%@",lines);
-                    NSLog(@"%@",message);
                 }
                 
                 else if (rtype==WORDS)
                 {
                     message = words;
-                    NSLog(@"%@",words);
-                    NSLog(@"%@",message);
                 }
                 
                 else if (rtype==ALL)
@@ -211,8 +153,6 @@
                     [all appendString:@("\n")];
                     [all appendString:words];
                     message = all;
-                    NSLog(@"%@",all);
-                    NSLog(@"%@",message);
                 }
                 else
                 {
@@ -221,7 +161,6 @@
                                                messageAsString:@"Return Type can only be 0,1,2 oe 3"];
                     [self.commandDelegate sendPluginResult:result callbackId:_commandglo.callbackId];
                 }
-                NSLog(@"%@",message);
                 CDVPluginResult* result = [CDVPluginResult
                                            resultWithStatus:CDVCommandStatus_OK
                                            messageAsString:message];
@@ -284,8 +223,6 @@
 
 -(NSData *)retrieveAssetDataPhotosFramework:(NSURL *)urlMedia
 {
-    NSLog(@"urlmediaiswahwah :- (%@)", urlMedia);
-    
     __block NSData *iData = nil;
     
     PHFetchResult *result = [PHAsset fetchAssetsWithALAssetURLs:@[urlMedia] options:nil];
@@ -299,10 +236,7 @@
         
         @autoreleasepool {
             [imageManager requestImageDataForAsset:asset options:options resultHandler:^(NSData *imageData, NSString *dataUTI, UIImageOrientation orientation, NSDictionary *info) {
-                iData = [imageData copy];
-                NSLog(@"requestImageDataForAsset returned info(%@)", iData);
-                NSLog(@"requestImageDataForAsset returned info(%@)", info);
-                
+                iData = [imageData copy];          
             }];
         }
         //assert(iData.length != 0);
