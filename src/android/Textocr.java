8c1e4dd1ba7d2e746ffb67e0e3357bdc15293b40
ChrisTomAlx <christythomasalex@gmail.com>
2019-02-19 00:36:49 +0530

    Changed android return and input values

--- src/android/Textocr.java
+++ src/android/Textocr.java
@@ -6,6 +6,7 @@ import org.apache.cordova.PluginResult;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.Manifest;
 import android.content.Context;
@@ -24,6 +25,7 @@ import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
 
 import java.io.FileNotFoundException;
+import java.util.Objects;
 
 
 public class Textocr extends CordovaPlugin {
@@ -36,28 +38,19 @@ public class Textocr extends CordovaPlugin {
     private static final int FASTNATIVEURI = 3; // Make compressed bitmap using uri from picture library for faster ocr but might reduce accuracy (FASTFILEURI & FASTFILEURI have same functionality in android)
     private static final int BASE64 = 4;  // send base64 image instead of uri
     private TextRecognizer detector;
-    private static final int BLOCKS = 0; // return blocks with 2 new lines in between
-    private static final int LINES = 1; // return lines with new line in between
-    private static final int WORDS = 2; // return words with comma in between
-    private static final int ALL = 3; // return all with new line in between
-    // protected final static String[] permissions = { Manifest.permission.CAMERA,
-    //         Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE };
 
     @Override
     public boolean execute(String action, final JSONArray args, final CallbackContext callbackContext) throws JSONException {
-        //callbackContext = callbackContext;
         if (action.equals("recText")) {
             cordova.getThreadPool().execute(new Runnable() {
                 public void run() {
                     try {
                             int argstype = NORMFILEURI;
-                            int argrtype = ALL;
                             String argimagestr = "";
                         try
                         {
                             argstype = args.getInt(0);
-                            argrtype = args.getInt(1);
-                            argimagestr = args.getString(2);
+                            argimagestr = args.getString(1);
                         }
                         catch(Exception e)
                         {
@@ -134,32 +127,185 @@ public class Textocr extends CordovaPlugin {
 
                         if (detector.isOperational() && bitmap != null)
                         {
-                            //Log.w("det", "Detector dependencies are not yet available.");
-                            // Check for low storage.  If there is low storage, the native library will not be
-                            // downloaded, so detection will not become operational.
-                            // TODO: 05-Jul-18 low storage check
-                            //                    IntentFilter lowstorageFilter = new IntentFilter(Intent.ACTION_DEVICE_STORAGE_LOW);
-                            //                    boolean hasLowStorage = registerReceiver(null, lowstorageFilter) != null;
-                            //
-                            //                    if (hasLowStorage) {
-                            //                        Toast.makeText(cordova.getContext(), "low storage", Toast.LENGTH_LONG).show();
-                            //                        Log.w("det","low storage");
-                            //                    }
                             Frame frame = new Frame.Builder().setBitmap(bitmap).build();
                             SparseArray<TextBlock> textBlocks = detector.detect(frame);
-                            String blocks = "";
-                            String lines = "";
-                            String words = "";
+
+                            JSONObject resultobj = new JSONObject();
+
+                            JSONObject blockobj = new JSONObject();
+                            JSONObject lineobj = new JSONObject();
+                            JSONObject wordobj = new JSONObject();
+
+                            JSONArray blocktext = new JSONArray();
+                            JSONArray blocklanguages = new JSONArray();
+                            JSONArray blockpoints = new JSONArray();
+                            JSONArray blockframe = new JSONArray();
+
+                            JSONArray linetext = new JSONArray();
+                            JSONArray linelanguages = new JSONArray();
+                            JSONArray linepoints = new JSONArray();
+                            JSONArray lineframe = new JSONArray();
+
+                            JSONArray wordtext = new JSONArray();
+                            JSONArray wordlanguages = new JSONArray();
+                            JSONArray wordpoints = new JSONArray();
+                            JSONArray wordframe = new JSONArray();
+
                             for (int index = 0; index < textBlocks.size(); index++) {
-                                //extract scanned text blocks here
+
                                 TextBlock tBlock = textBlocks.valueAt(index);
-                                blocks = blocks + tBlock.getValue() + "\n" + "\n";
-                                for (Text line : tBlock.getComponents()) {
-                                    //extract scanned text lines here
-                                    lines = lines + line.getValue() + "\n";
-                                    for (Text element : line.getComponents()) {
-                                        //extract scanned text words here
-                                        words = words + element.getValue() + ", ";
+
+                                blocktext.put(tBlock.getValue());
+                                blocklanguages.put(tBlock.getLanguage());
+
+                                JSONObject blockcorners = new JSONObject();
+                                if (tBlock.getCornerPoints()==null){
+                                    blockcorners.put("x1", "");
+                                    blockcorners.put("y1", "");
+
+                                    blockcorners.put("x2", "");
+                                    blockcorners.put("y2", "");
+
+                                    blockcorners.put("x3", "");
+                                    blockcorners.put("y3", "");
+
+                                    blockcorners.put("x4", "");
+                                    blockcorners.put("y4", "");
+                                }
+                                else {
+                                    blockcorners.put("x1", Objects.requireNonNull(tBlock.getCornerPoints())[0].x);
+                                    blockcorners.put("y1", Objects.requireNonNull(tBlock.getCornerPoints())[0].y);
+
+                                    blockcorners.put("x2", Objects.requireNonNull(tBlock.getCornerPoints())[1].x);
+                                    blockcorners.put("y2", Objects.requireNonNull(tBlock.getCornerPoints())[1].y);
+
+                                    blockcorners.put("x3", Objects.requireNonNull(tBlock.getCornerPoints())[2].x);
+                                    blockcorners.put("y3", Objects.requireNonNull(tBlock.getCornerPoints())[2].y);
+
+                                    blockcorners.put("x4", Objects.requireNonNull(tBlock.getCornerPoints())[3].x);
+                                    blockcorners.put("y4", Objects.requireNonNull(tBlock.getCornerPoints())[3].y);
+                                }
+                                blockpoints.put(blockcorners);
+
+                                JSONObject blockframeobj = new JSONObject();
+                                if (tBlock.getBoundingBox()==null)
+                                {
+                                    blockframeobj.put("x", "");
+                                    blockframeobj.put("y", "");
+                                    blockframeobj.put("height","");
+                                    blockframeobj.put("width", "");
+                                }
+                                else {
+                                    blockframeobj.put("x", tBlock.getBoundingBox().left);
+                                    blockframeobj.put("y", tBlock.getBoundingBox().bottom);
+                                    blockframeobj.put("height", tBlock.getBoundingBox().height());
+                                    blockframeobj.put("width", tBlock.getBoundingBox().width());
+                                }
+                                blockframe.put(blockframeobj);
+
+                                for (Text line : tBlock.getComponents())
+                                {
+                                    linetext.put(line.getValue());
+                                    linelanguages.put(tBlock.getLanguage());
+
+                                    JSONObject linecorners = new JSONObject();
+                                    if (line.getCornerPoints()==null){
+                                        linecorners.put("x1", "");
+                                        linecorners.put("y1", "");
+
+                                        linecorners.put("x2", "");
+                                        linecorners.put("y2", "");
+
+                                        linecorners.put("x3", "");
+                                        linecorners.put("y3", "");
+
+                                        linecorners.put("x4", "");
+                                        linecorners.put("y4", "");
+                                    }
+                                    else {
+                                        linecorners.put("x1", line.getCornerPoints()[0].x);
+                                        linecorners.put("y1", line.getCornerPoints()[0].y);
+
+                                        linecorners.put("x2", line.getCornerPoints()[1].x);
+                                        linecorners.put("y2", line.getCornerPoints()[1].y);
+
+                                        linecorners.put("x3", line.getCornerPoints()[2].x);
+                                        linecorners.put("y3", line.getCornerPoints()[2].y);
+
+                                        linecorners.put("x4", line.getCornerPoints()[3].x);
+                                        linecorners.put("y4", line.getCornerPoints()[3].y);
+                                    }
+                                    linepoints.put(linecorners);
+
+                                    JSONObject lineframeobj = new JSONObject();
+                                    if (line.getBoundingBox()==null)
+                                    {
+                                        lineframeobj.put("x", "");
+                                        lineframeobj.put("y", "");
+                                        lineframeobj.put("height","");
+                                        lineframeobj.put("width", "");
+                                    }
+                                    else
+                                    {
+                                        lineframeobj.put("x", line.getBoundingBox().left);
+                                        lineframeobj.put("y", line.getBoundingBox().bottom);
+                                        lineframeobj.put("height", line.getBoundingBox().height());
+                                        lineframeobj.put("width", line.getBoundingBox().width());
+                                    }
+                                    lineframe.put(lineframeobj);
+
+                                    for (Text element : line.getComponents())
+                                    {
+                                        wordtext.put(element.getValue());
+                                        wordlanguages.put(tBlock.getLanguage());
+
+                                        JSONObject wordcorners = new JSONObject();
+                                        if (element.getCornerPoints()==null)
+                                        {
+                                            wordcorners.put("x1", "");
+                                            wordcorners.put("y1", "");
+
+                                            wordcorners.put("x2", "");
+                                            wordcorners.put("y2", "");
+
+                                            wordcorners.put("x3", "");
+                                            wordcorners.put("y3", "");
+
+                                            wordcorners.put("x4", "");
+                                            wordcorners.put("y4", "");
+                                        }
+                                        else
+                                        {
+                                            wordcorners.put("x1", element.getCornerPoints()[0].x);
+                                            wordcorners.put("y1", element.getCornerPoints()[0].y);
+
+                                            wordcorners.put("x2", element.getCornerPoints()[1].x);
+                                            wordcorners.put("y2", element.getCornerPoints()[1].y);
+
+                                            wordcorners.put("x3", element.getCornerPoints()[2].x);
+                                            wordcorners.put("y3", element.getCornerPoints()[2].y);
+
+                                            wordcorners.put("x4", element.getCornerPoints()[3].x);
+                                            wordcorners.put("y4", element.getCornerPoints()[3].y);
+                                        }
+                                        wordpoints.put(wordcorners);
+
+                                        JSONObject wordframeobj = new JSONObject();
+                                        if (element.getBoundingBox()==null)
+                                        {
+                                            wordframeobj.put("x", "");
+                                            wordframeobj.put("y", "");
+                                            wordframeobj.put("height","");
+                                            wordframeobj.put("width", "");
+                                        }
+                                        else
+                                        {
+                                            wordframeobj.put("x", element.getBoundingBox().left);
+                                            wordframeobj.put("y", element.getBoundingBox().bottom);
+                                            wordframeobj.put("height", element.getBoundingBox().height());
+                                            wordframeobj.put("width", element.getBoundingBox().width());
+                                        }
+                                        wordframe.put(wordframeobj);
                                     }
                                 }
                             }
@@ -171,34 +317,26 @@ public class Textocr extends CordovaPlugin {
                             }
                             else
                             {
-                                String result="";
-                                if(argrtype==BLOCKS)
-                                {
-                                    result= blocks;
-                                }
-                                else if(argrtype==LINES)
-                                {
-                                    result= lines;
-                                }
-                                else if(argrtype==WORDS)
-                                {
-                                    result= words;
-                                }
-                                else if(argrtype==ALL)
-                                {
-                                    result= blocks + "\n" + lines + "\n" + words;
-                                }
-                                else
-                                {
-                                    callbackContext.error("Non existent returnType argument. Use 0, 1, 2 or 3");
-                                    PluginResult r = new PluginResult(PluginResult.Status.ERROR);
-                                    callbackContext.sendPluginResult(r);
-                                }
+                                blockobj.put("blocktext", blocktext);
+                                blockobj.put("blocklanguages", blocklanguages);
+                                blockobj.put("blockpoints", blockpoints);
+                                blockobj.put("blockframe", blockframe);
 
+                                lineobj.put("linetext", linetext);
+                                lineobj.put("linelanguages", linelanguages);
+                                lineobj.put("linepoints", linepoints);
+                                lineobj.put("lineframe", lineframe);
 
-                                //String result= blocks + "" + lines + "" + words;
-                                Log.v("cor", "Result: " + result);
-                                callbackContext.success(result);
+                                wordobj.put("wordtext", wordtext);
+                                wordobj.put("wordlanguages", wordlanguages);
+                                wordobj.put("wordpoints", wordpoints);
+                                wordobj.put("wordframe", wordframe);
+
+                                resultobj.put("blocks", blockobj);
+                                resultobj.put("lines", lineobj);
+                                resultobj.put("words", wordobj);
+
+                                callbackContext.success(resultobj);
                                 PluginResult r = new PluginResult(PluginResult.Status.OK);
                                 callbackContext.sendPluginResult(r);
                             }
@@ -231,25 +369,9 @@ public class Textocr extends CordovaPlugin {
             return true;
 
         }
-        //else
-        //{
             return false;
-        //}
     }
 
-//    @Override
-//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//        if (requestCode == REQUEST_CODE && resultCode == cordova.getActivity().RESULT_OK) {
-//           // Uri uri = data.getExtras().getParcelable(ScanConstants.SCANNED_RESULT);
-//            if (uri != null) {
-//                String fileLocation = FileHelper.getRealPath(uri, this.cordova);
-//                this.callbackContext.success("file://" + fileLocation);
-//            } else {
-//                this.callbackContext.error("null data from scan libary");
-//            }
-//        }
-//    }
 
     private Bitmap decodeBitmapUri(Context ctx, Uri uri) throws FileNotFoundException
     {
--- www/rectext.js
+++ www/rectext.js
@@ -1,7 +1,7 @@
 /*global cordova, module*/
 
 module.exports = {
-    recText: function (sourceType, returnType, imageSource, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Textocr", "recText", [sourceType, returnType, imageSource]);
+    recText: function (sourceType, imageSource, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Textocr", "recText", [sourceType, imageSource]);
     }
 };
